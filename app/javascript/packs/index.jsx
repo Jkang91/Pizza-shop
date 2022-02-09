53fa5fa0fcd2cdf4edde41ca7daf4123ad0c9091
jkang91 <jkang34@gmail.com>
2022-02-09 15:21:40 -0500

    holy shit I did it

--- app/javascript/packs/index.jsx
+++ app/javascript/packs/index.jsx
@@ -3,9 +3,8 @@
 // of the page.
 
 import React from 'react'
-import ReactDOM from 'react-dom'
-import PropTypes from 'prop-types'
-import App from '../components/App'
+import ReactDOM from 'react-dom';
+import App from '../components/App';
 
 
 document.addEventListener('DOMContentLoaded', () => {
