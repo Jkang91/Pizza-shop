6c6f8858d3da15cbf1ba3f6e8db4fa239af2bbb7
jkang91 <jkang34@gmail.com>
2022-02-23 12:57:35 -0500

    changed css

--- app/javascript/components/Navbar.js
+++ app/javascript/components/Navbar.js
@@ -2,10 +2,12 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import "../App.css";
 
-
 function Navbar() {
   return (
     <div className="nav-bar">
+      <div className="link">
+        <NavLink to="/">Home</NavLink>
+      </div>
       <div className="link">
         <NavLink to="/diff_pizzas">Menu</NavLink>
       </div>
--- app/javascript/components/Pizza.js
+++ app/javascript/components/Pizza.js
@@ -1,13 +1,14 @@
 import React from "react";
 
 function Pizza(pizza) {
-  console.log(pizza.pizza);
+  console.log(pizza);
   return (
     <div className="pizza">
       <img src={pizza.pizza.image_url} alt={pizza.pizza.name}/>
       <div>Name: {pizza.pizza.name}</div>
       <div>Price: {pizza.pizza.price}</div>
       <div>Description: {pizza.pizza.description}</div>
+      <div>Reviews: {pizza.pizza.reviews}</div>
     </div>
   );
 }
